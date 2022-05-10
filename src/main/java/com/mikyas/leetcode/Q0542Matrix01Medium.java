2163a68294c7151f27eb556fbfdfd30af0d37b30
tmikyas <mtarikmikyas@gmail.com>
2022-05-10 22:29:55 +0300

    Answer of 542-695-733-994.

---
+++ src/main/java/com/mikyas/leetcode/Q0542Matrix01Medium.java
@@ -0,0 +1,72 @@
+package com.mikyas.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+ * <p>
+ * The distance between two adjacent cells is 1.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+ * Output: [[0,0,0],[0,1,0],[0,0,0]]
+ * Example 2:
+ * <p>
+ * <p>
+ * Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+ * Output: [[0,0,0],[0,1,0],[1,2,1]]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * m == mat.length
+ * n == mat[i].length
+ * 1 <= m, n <= 104
+ * 1 <= m * n <= 104
+ * mat[i][j] is either 0 or 1.
+ * There is at least one 0 in mat.
+ */
+public class Q0542Matrix01Medium {
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                } else {
+                    mat[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            for (int[] d : dirs) {
+                int r = cell[0] + d[0];
+                int c = cell[1] + d[1];
+                if (r < 0 || r >= m || c < 0 || c >= n ||
+                        mat[r][c] <= mat[cell[0]][cell[1]] + 1) continue;
+                queue.add(new int[]{r, c});
+                mat[r][c] = mat[cell[0]][cell[1]] + 1;
+            }
+        }
+
+        return mat;
+
+    }
+}
